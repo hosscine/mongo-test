5586f06ff737818972757f30acbf98763a138cfa
hosscine <futagamirentaro@gmail.com>
2019-09-18 23:14:32 +0900

    remove try catch

--- app.js
+++ app.js
@@ -7,11 +7,12 @@ const url = 'mongodb://api:hoge22@localhost:27017/?authMechanism=SCRAM-SHA-1&aut
 const dbName = 'laxury'
 const collectionName = "bill"
 
-// get connection
+// Setup express
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+// Get collection
 let collection
 const connectOption = {
   useNewUrlParser: true,
@@ -27,39 +28,33 @@ MongoClient.connect(url, connectOption, (err, client) => {
   app.listen(3000, () => console.log('server starts listening 3000'))
 })
 
-app.get('/laxury/latest', async (req, res) => {
+app.get('/laxury/latest', (req, res) => {
   const option = { $group: { _id: '', last: { $max: "$_id" } } }
-  try {
-    const bill = await collection.aggregate(option).toArray()
-    console.log(bill)
-    res.send(bill)
-  } catch (err) {
-    console.log(err)
-    res.status(500).send()
-  }
+  collection.aggregate(option).toArray()
+    .then(bill => res.send(bill))
+    .catch(err => {
+      console.log(err)
+      res.status(500).send()
+    })
 })
 
-app.get('/laxury/all', async (req, res) => {
-  const option = { $group: { _id: '', last: { $max: "$_id" } } }
-  try {
-    const bill = await collection.find({}).toArray()
-    res.send(bill)
-  } catch (err) {
-    console.log(err)
-    res.status(500).send()
-  }
+app.get('/laxury/all', (req, res) => {
+  collection.find({}).toArray()
+    .then(bill => res.send(bill))
+    .catch(err => {
+      console.log(err)
+      res.status(500).send()
+    })
 })
 
-app.post('/laxury/regist', async (req, res) => {
+app.post('/laxury/regist', (req, res) => {
   const date = req.body.date
   const item = req.body.item
-  try {
-    collection.insertOne({ _id: date, item: item })
-    res.render('regist', { action: req.path, message: 'registration is success' }).send()
-  } catch (err) {
-    console.log(err, err.code)
-    if (err.code == 11000) res.status(208).send("already registered")
-    else res.status(500).send()
-    res.status(208).send("already registered")
-  }
+  collection.insertOne({ _id: date, name: item })
+    .then(() => res.send('registration succeeded'))
+    .catch(err => {
+      console.log(err)
+      if (err.code == 11000) res.send('already registered')
+      else res.status(500).send()
+    })
 })
